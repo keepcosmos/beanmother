99f566400421bd0d9f955a5f49245524366511b8
keepcosmos <keepcosmos@gmail.com>
2017-09-11 14:45:35 +0900

    remove ObjectMotherTest

--- beanmother-core/src/test/java/io/beanmother/core/AbstractBeanMotherTest.java
+++ beanmother-core/src/test/java/io/beanmother/core/AbstractBeanMotherTest.java
@@ -1,27 +1,37 @@
 package io.beanmother.core;
 
-import io.beanmother.testmodel.Author;
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.List;
 
 public class AbstractBeanMotherTest {
 
-    BeanMother beanMother = new AbstractBeanMother() {};
+    static class TestObjectMother extends AbstractBeanMother {
 
-    @Before
-    public void setup() {
-        // path from beanmother-testmodel module
-        beanMother.addFixtureLocation("testmodel_fixtures");
+        private final static TestObjectMother beanMother = new AbstractBeanMotherTest.TestObjectMother();
+
+        public static TestObjectMother getInstance() {
+            return beanMother;
+        }
+
+        private TestObjectMother() {
+            super();
+        }
+
+        @Override
+        public String[] defaultFixturePaths() {
+            return new String[]{ "testmodel_fixtures" };
+        }
     }
 
+    TestObjectMother beanMother = TestObjectMother.getInstance();
+
     @Test
+    @Ignore
     public void testMapping() {
 //        Author author = beanMother.bear("hemingway", Author.class);
 //        System.out.println(author);
 
-        List<Author> authors = beanMother.bear("unknown_author", Author.class, 5);
-        System.out.println(authors);
+//        List<Author> authors = beanMother.bear("unknown_author", Author.class, 5);
+//        System.out.println(authors);
     }
 }
